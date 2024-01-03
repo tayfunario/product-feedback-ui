42d66e78812b5eef2a862c843dd8ba0bf2f15ebc
tayfunetta <tayfunemrekahraman@gmail.com>
2024-01-03 21:37:49 +0300

    refactored <Comment /> and completed <Reply />

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -19,8 +19,8 @@ export default function Comment({
           className="w-11 h-11 rounded-full"
         />
         <div className="ml-4">
-          <h3 className="bold-13 text-3A4">Ali Bravo</h3>
-          <p className="text-[13px] text-647">26 August 2020</p>
+          <h3 className="bold-13 text-3A4">{user_name}</h3>
+          <p className="text-[13px] text-647">@{nick_name}</p>
         </div>
         <button
           className="ml-auto text-466 body-3"
@@ -30,15 +30,14 @@ export default function Comment({
         </button>
       </div>
 
-      <p className="text-647 mt-5">
-        We need this feature too! Having folders seems like a basic feature that
-        is available on most apps similar to this one.
+      <p className="text-647 mt-5 text-[13px]">
+        {content}
       </p>
       {openComment && (
         <div className="text-end">
           <input
             type="textarea"
-            className="w-full mt-5 px-3 py-2 bg-F7F text-3A4 rounded-lg border border-transparent focus:border-466 focus:outline-none"
+            className="w-full mt-5 px-3 py-2 text-[13px] bg-F7F text-3A4 rounded-lg border border-transparent focus:border-466 focus:outline-none"
             placeholder="Reply..."
           />
           <button className="button-1 mt-3">Post Reply</button>
--- components/Reply.tsx
+++ components/Reply.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function Reply({
   comment_id,
   content,
@@ -6,8 +8,9 @@ export default function Reply({
   user_image,
   user_name,
 }: ReplyProps) {
+  const [openComment, setOpenComment] = useState<boolean>(false);
   return (
-    <div className="py-3 border-l pl-5">
+    <div className="pt-3 border-l pl-5">
       <div className="flex items-center">
         <img
           src={`/user-images/${user_image}`}
@@ -15,16 +18,31 @@ export default function Reply({
           className="w-11 h-11 rounded-full"
         />
         <div className="ml-4">
-          <h3 className="bold-13 text-3A4">Ali Bravo</h3>
-          <p className="text-[13px] text-647">26 August 2020</p>
+          <h3 className="bold-13 text-3A4">{user_name}</h3>
+          <p className="text-[13px] text-647">@{nick_name}</p>
         </div>
-        <button className="ml-auto text-466 body-3">Reply</button>
+        <button
+          className="ml-auto text-466 body-3"
+          onClick={() => setOpenComment(true)}
+        >
+          Reply
+        </button>
       </div>
 
-      <p className="text-647 mt-5">
-        We need this feature too! Having folders seems like a basic feature that
-        is available on most apps similar to this one.
+      <p className="text-647 mt-5 text-[13px]">
+        <span className="bold-13 text-AD1">@{replying_to} </span>
+        {content}
       </p>
+      {openComment && (
+        <div className="text-end">
+          <input
+            type="textarea"
+            className="w-full mt-5 px-3 py-2 text-[13px] bg-F7F text-3A4 rounded-lg border border-transparent focus:border-466 focus:outline-none"
+            placeholder="Reply..."
+          />
+          <button className="button-1 mt-3">Post Reply</button>
+        </div>
+      )}
     </div>
   );
 }
