55cae0e37efebd60066a55ba485f3542ea6928d8
tayfunetta <tayfunemrekahraman@gmail.com>
2024-01-06 15:16:12 +0300

    completed switch

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -2,8 +2,10 @@ import Layout from "../components/Layout";
 import Link from "next/link";
 import { TiPlus } from "react-icons/ti";
 import { FaChevronLeft } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Roadmap() {
+  const [chosen, setChosen] = useState<string>("planned");
   return (
     <Layout>
       <div className="flex justify-between items-center p-5 bg-373">
@@ -20,6 +22,33 @@ export default function Roadmap() {
         <button className="button-1 flex justify-center items-center my-2">
           <TiPlus className="mr-1" />
           Add Feedback
+        </button>
+      </div>
+
+      <div className="flex h-14 border-b">
+        <button
+          className={`w-full bold-13 text-3A4 border-b-4 ${
+            chosen === "planned" ? "border-F49" : "border-transparent opacity-40"
+          }`}
+          onClick={() => setChosen("planned")}
+        >
+          Planned (2)
+        </button>
+        <button
+          className={`w-full bold-13 text-3A4 border-b-4 ${
+            chosen === "in-progress" ? "border-AD1" : "border-transparent opacity-40"
+          }`}
+          onClick={() => setChosen("in-progress")}
+        >
+          In-Progress (3)
+        </button>
+        <button
+          className={`w-full bold-13 text-3A4 border-b-4 ${
+            chosen === "live" ? "border-62B" : "border-transparent opacity-40"
+          }`}
+          onClick={() => setChosen("live")}
+        >
+          Live (1)
         </button>
       </div>
     </Layout>
