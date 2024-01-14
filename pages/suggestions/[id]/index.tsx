fc462eefe84bd3ab9f7edaae01a1c019620f0f54
tayfunetta <tayfunemrekahraman@gmail.com>
2024-01-14 15:09:21 +0300

    Added comment functionality and input validation

--- pages/suggestions/[id]/index.tsx
+++ pages/suggestions/[id]/index.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Layout from "../../../components/Layout";
 import Suggestion from "../../../components/Suggestion";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Comment from "../../../components/Comment";
+import { useRouter } from "next/router";
 
 export default function Detail({ data }) {
   const [totalComments, setTotalComments] = useState<number>(
@@ -10,11 +11,51 @@ export default function Detail({ data }) {
   );
   const [newComment, setNewComment] = useState<string>("");
   const [width, setWidth] = useState<number>(0);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
 
   useEffect(() => {
     setWidth(window.innerWidth);
     window.addEventListener("resize", () => setWidth(window.innerWidth));
   }, []);
+
+  const checkCommentInput = () => {
+    let isInputValid = true;
+    if (textAreaRef.current!.value === "") {
+      isInputValid = false;
+      textAreaRef.current!.classList.add("border-red-500");
+      textAreaRef.current!.classList.remove("border-transparent");
+      textAreaRef.current!.nextElementSibling!.classList.remove("invisible");
+    }
+
+    if (isInputValid) {
+      sendCommentToServer();
+      setNewComment("");
+    }
+  };
+
+  const resetTextAreaStyle = () => {
+    textAreaRef.current!.classList.remove("border-red-500");
+    textAreaRef.current!.nextElementSibling!.classList.add("invisible");
+  };
+
+  const sendCommentToServer = async () => {
+    const res = await fetch(`http://localhost:4000/comment`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: newComment,
+        user_image: "image-guest.webp",
+        user_name: "Guest",
+        nick_name: "guest01",
+        request_id: data.suggestion.id,
+      }),
+    });
+
+    router.reload();
+  };
 
   return width < 768 ? (
     <Layout>
@@ -61,18 +102,27 @@ export default function Detail({ data }) {
         <footer className="bg-white rounded-lg mt-5 p-5">
           <h2 className="h3-bold">Add Comment</h2>
           <textarea
+            ref={textAreaRef}
             className="w-full mt-5 px-3 py-2 text-[13px] bg-F7F text-3A4 rounded-lg border border-transparent focus:border-466 focus:outline-none"
             placeholder="Type your comment here"
             value={newComment}
-            onChange={(e) => setNewComment(e.target.value)}
+            onChange={(e) => {
+              setNewComment(e.target.value);
+              resetTextAreaStyle();
+            }}
             rows={3}
             maxLength={250}
           />
+          <p className="invisible md:text-xs text-[10px] mt-[-7px] text-[#D73737]">
+            Can't be empty
+          </p>
           <div className="flex justify-between items-center mt-3">
             <span className="text-[13px] text-647">
               {250 - newComment.length} Characters left
             </span>
-            <button className="button-1">Post Comment</button>
+            <button className="button-1" onClick={() => checkCommentInput()}>
+              Post Comment
+            </button>
           </div>
         </footer>
       </div>
@@ -123,18 +173,27 @@ export default function Detail({ data }) {
         <footer className="bg-white rounded-lg mt-5 p-5">
           <h2 className="h3-bold">Add Comment</h2>
           <textarea
+            ref={textAreaRef}
             className="w-full mt-5 px-3 py-2 text-[15px] bg-F7F text-3A4 rounded-lg border border-transparent focus:border-466 focus:outline-none"
             placeholder="Type your comment here"
             value={newComment}
-            onChange={(e) => setNewComment(e.target.value)}
+            onChange={(e) => {
+              setNewComment(e.target.value);
+              resetTextAreaStyle();
+            }}
             rows={3}
             maxLength={250}
           />
+          <p className="invisible md:text-xs text-[10px] mt-[-7px] text-[#D73737]">
+            Can't be empty
+          </p>
           <div className="flex justify-between items-center mt-3">
             <span className="text-[15px] text-647">
               {250 - newComment.length} Characters left
             </span>
-            <button className="button-1">Post Comment</button>
+            <button className="button-1" onClick={() => checkCommentInput()}>
+              Post Comment
+            </button>
           </div>
         </footer>
       </div>
